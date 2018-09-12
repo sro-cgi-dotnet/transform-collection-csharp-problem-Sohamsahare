d8b41cc83fa3cb801f046e704c87e04779b65fa9
Soham Sahare <sohamsahare@gmail.com>
2018-09-12 14:42:02 +0530

    added comments

--- Transform/Transform.cs
+++ Transform/Transform.cs
@@ -4,11 +4,13 @@ using System.Collections.Generic;
 
 namespace Transform
 {
+    // Delegate that takes the lamba expression
     public delegate int ExpressionDelegate(int number);
     public static class Transform 
     {
         public static int[] Map(this int[] numbers, ExpressionDelegate expressionDelegate)
         {
+            // individually transform elements of the array
             int[] result = new int[numbers.Length];
             for(int i = 0; i < result.Length; i++){
                 result[i] = expressionDelegate(numbers[i]);
