9bac26641969705e7d6692ef8e981779ce90dd47
Soham Sahare <sohamsahare@gmail.com>
2018-09-12 16:10:48 +0530

    Used IEnumerable and Select to implement the same function

--- Transform/Transform.cs
+++ Transform/Transform.cs
@@ -4,18 +4,24 @@ using System.Collections.Generic;
 
 namespace Transform
 {
-    // Delegate that takes the lamba expression
-    public delegate int ExpressionDelegate(int number);
-    public static class Transform 
-    {
-        public static int[] Map(this int[] numbers, ExpressionDelegate expressionDelegate)
-        {
-            // individually transform elements of the array
-            int[] result = new int[numbers.Length];
-            for(int i = 0; i < result.Length; i++){
-                result[i] = expressionDelegate(numbers[i]);
-            }
-            return result;
-        }
+    // // Delegate that takes the lamba expression
+    // public delegate int ExpressionDelegate(int number);
+    // public static class Transform 
+    // {
+    //     public static int[] Map(this int[] numbers, ExpressionDelegate expressionDelegate)
+    //     {
+    //         // individually transform elements of the array
+    //         int[] result = new int[numbers.Length];
+    //         for(int i = 0; i < result.Length; i++){
+    //             result[i] = expressionDelegate(numbers[i]);
+    //         }
+    //         return result;
+    //     }
+    // }
+
+    public static class Transform{
+        public static IEnumerable<TResult> Map<TSource, TResult>(this IEnumerable<TSource> input,Func<TSource,TResult> expressionFunc){
+            return input.Select(expressionFunc);
+        } 
     }
 }
